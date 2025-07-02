5e3dd07eef68a9b37ef062364dbd88cddd56432c
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-07-02 08:48:04 +0700

    Java Fundamental

---
+++ Java Fundamental Pt4/moklet.java
@@ -0,0 +1,31 @@
+public class moklet {
+    public static void main(String[] args) {
+        int hari = 2;
+
+        switch (hari) {
+            case 1:
+                System.out.println("Hari Senin");
+                break;
+            case 2:
+                System.out.println("Hari Selasa");
+                break;
+            case 3:
+                System.out.println("Hari Rabu");
+                break;
+            case 4:
+                System.out.println("Hari Kamis");
+                break;
+            case 5:
+                System.out.println("Hari Jumat");
+                break;
+            case 6:
+                System.out.println("Hari Sabtu");
+                break;
+            case 7:
+                System.out.println("Hari Minggu");
+                break;
+            default:
+                System.out.println("Tidak ada hari tersebut");
+        }
+    }
+}
